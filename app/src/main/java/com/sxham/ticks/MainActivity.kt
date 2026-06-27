bd69459bb9619983d8352cf15975b6fbb14cf1d9
Soham <sharma.soham04@gmail.com>
2026-06-27 19:07:42 +0530

    add different states for dots, completed, incomplete and invalid

--- app/src/main/java/com/sxham/ticks/MainActivity.kt
+++ app/src/main/java/com/sxham/ticks/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.sxham.ticks.data.WidgetPreferences
 import com.sxham.ticks.ui.CalendarAdapter
 import com.sxham.ticks.ui.CalendarBuilder
+import com.sxham.ticks.ui.CalendarItem
 import com.sxham.ticks.ui.ConfigActivity
 import com.sxham.ticks.ui.MonthGapDecoration
 
@@ -44,26 +45,28 @@ class MainActivity : AppCompatActivity() {
     private fun buildCalendar() {
         val startDate = WidgetPreferences.getStartDate(this, appWidgetId)
         val endDate   = WidgetPreferences.getEndDate(this, appWidgetId)
-
-        val items = CalendarBuilder.build(startDate, endDate)
+        val items     = CalendarBuilder.build(startDate, endDate)
 
         val recyclerView = findViewById<RecyclerView>(R.id.rv_calendar_grid)
 
-        recyclerView.layoutManager = GridLayoutManager(
-            this,
-            CalendarBuilder.SPAN_COUNT,
-            GridLayoutManager.HORIZONTAL,
-            false
-        )
+        val spanCount = CalendarBuilder.SPAN_COUNT
 
-        // Clear any previously attached decorations before re-adding
+        val layoutManager = GridLayoutManager(this, spanCount, GridLayoutManager.HORIZONTAL, false)
+
+        // Month labels span 3 columns so the text has room to render fully.
+        // All other cells span 1 column as normal.
+        layoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                return if (items[position] is CalendarItem.MonthLabel) 3 else 1
+            }
+        }
+
         while (recyclerView.itemDecorationCount > 0) {
             recyclerView.removeItemDecorationAt(0)
         }
-        recyclerView.addItemDecoration(
-            MonthGapDecoration(this, items, CalendarBuilder.SPAN_COUNT)
-        )
+        recyclerView.addItemDecoration(MonthGapDecoration(this, items, spanCount))
 
+        recyclerView.layoutManager = layoutManager
         recyclerView.adapter = CalendarAdapter(items, startDate, endDate)
     }
 }
